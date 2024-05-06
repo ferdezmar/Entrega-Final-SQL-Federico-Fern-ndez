e718411e34de71f6798ed433c63c6d5a87cb7bb8
ferdezmar <114118826+ferdezmar@users.noreply.github.com>
2024-05-06 00:37:36 -0300

    Update roles_users.sql

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -8,6 +8,16 @@ FLUSH PRIVILEGES;
 
 USE venta_libros;
 
+CREATE USER 'empleado'@'%' IDENTIFIED BY 'password_empleado';
+GRANT SELECT ON venta_libros.* TO 'empleado'@'%';
+
+CREATE USER 'gerente'@'%' IDENTIFIED BY 'password_gerente';
+GRANT ALL ON venta_libros.* TO 'gerente'@'%';
+
+/*
+Se intentó realizar el siguiente script de asignación de roles sin lograr que se asignaran correctamente, 
+por ello se decidió dar los privilegios directamente a los usuarios.
+
 -- CREACIÓN DE ROLES
 CREATE ROLE role_select_vistas;
 CREATE ROLE role_crud_sucursales;
@@ -35,3 +45,4 @@ CREATE USER 'gerente'@'%' IDENTIFIED BY 'password_gerente';
 GRANT role_crud_sucursales TO 'gerente'@'%';
 
 FLUSH PRIVILEGES;
+*/
